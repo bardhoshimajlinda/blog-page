30403034f50334ba1cdeec2f44760c74ae0637ec
bardhoshimajlinda <116483572+bardhoshimajlinda@users.noreply.github.com>
2024-12-24 22:51:54 +0100

    page not found style

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import notFound from "./../assets/404.jpg";
 
 const NotFound = () => {
   return (
     <div className="not-found">
-      <h2>Sorry</h2>
+      <img src={notFound} />
       <p>That page cannot be found</p>
       <Link to="/">Back to the homepage...</Link>
     </div>
