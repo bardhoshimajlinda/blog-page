f84081dc0050ae3f924befe8a7c0f9d543156ec2
bardhoshimajlinda <116483572+bardhoshimajlinda@users.noreply.github.com>
2024-12-25 21:52:40 +0100

    context

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -47,7 +47,7 @@ const Create = () => {
           type="text"
           required
           value={author}
-          onChange={(e) => setAuthor(e.target.value)}
+          onChan={(e) => setAuthor(e.target.value)}
         />
         {!isPending && <button>Add blog</button>}
         {isPending && <button disabled>Adding blog...</button>}
